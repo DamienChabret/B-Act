a255fa9c573337ed4031cdd26f90ddbf64d528c8
Damien CHABRET <104843206+DamienChabret@users.noreply.github.com>
2024-01-29 20:48:48 +0100

    re archi

---
+++ B-Act/Model/Interface/IRecipeDAO.cs
@@ -0,0 +1,46 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Interface
+{
+    /// <summary>
+    /// Interface for interact with the database
+    /// </summary>
+    public interface IRecipeDAO
+    {
+        /// <summary>
+        /// add a new recipe
+        /// </summary>
+        /// <param name="recipe"> recipe to add</param>
+        public void AddRecipe(Recipe recipe);
+
+        /// <summary>
+        /// Delete a recipe
+        /// </summary>
+        /// <param name="id"> id of the recipe to delete </param>
+        public void DeleteRecipe(int id);
+
+        /// <summary>
+        /// Update a recipe
+        /// </summary>
+        /// <param name="recipe"> recipe to update </param>
+        public Recipe UpdateRecipe(Recipe recipe);
+
+        /// <summary>
+        /// Get one recipe
+        /// </summary>
+        /// <param name="id"> id of the recipe </param>
+        /// <returns> A recipe </returns>
+        public Recipe GetRecipe(int id);
+
+        /// <summary>
+        /// Get all the recipes
+        /// </summary>
+        /// <returns> all recipe existing </returns>
+        public List<Recipe> GetRAllRecipe();
+    }
+}
---
+++ B-Act/Model/Recipe.cs
@@ -0,0 +1,58 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Class for a recipe
+    /// </summary>
+    public class Recipe
+    {
+        private int id;
+        private string name;
+        private string description;
+        private int rating;
+
+        /// <summary>
+        /// Natural constructor of the recipe
+        /// </summary>
+        public Recipe()
+        {
+            this.name = string.Empty;
+            this.description = string.Empty;
+        }
+
+        /// <summary>
+        /// ID of the recipe
+        /// </summary>
+        public int ID 
+        { 
+            get => id;
+            set => id = value; 
+        }
+
+        /// <summary>
+        /// Name of the recipe
+        /// </summary>
+        public string Name 
+        { 
+            get => name; 
+            set => name = value; 
+        }
+
+        /// <summary>
+        /// Description of how to make the recipe
+        /// </summary>
+        public string Description 
+        { 
+            get => description; 
+            set => description = value; 
+        }
+
+        /// <summary>
+        /// Rating given by the user to the recipe
+        /// </summary>
+        public int Rating 
+        { 
+            get => rating; 
+            set => rating = value; 
+        }
+    }
+}
